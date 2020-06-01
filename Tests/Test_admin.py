623dfc6b78f7be6c769485138def1c7d3c58b12d
AshithaL <47769140+AshithaL@users.noreply.github.com>
2020-06-01 09:13:43 +0530

    Updated Test cases

--- Tests/Test_admin.py
+++ Tests/Test_admin.py
@@ -6,25 +6,25 @@ from mock import patch
 class Test_admin(unittest.TestCase):
 
     def testCheckTotalBooking(self):
-        with patch('Admin.sqlite3') as test:
+        with patch('Admin') as test:
             test.connect().cursor().fetchall().return_value = ['cab.db']
             X = bool(Admin.check_total_bookings)
             assert(X, "Success")
 
     def testCheckEmpBooking(self):
-        with patch('Admin.sqlite3') as test:
+        with patch('Admin') as test:
             test.connect().cursor().fetchall().return_value = ['cab.db']
             X = bool(Admin.check_emp_booking)
             assert(X, "Success")
 
     def testAddNewCab(self):
-        with patch('Admin.sqlite3') as test:
+        with patch('Admin') as test:
             test.connect().cursor().fetchall().return_value = ['aims.db']
             X = bool(Admin.add_new_cab)
             assert(X, "Success")
 
     def testUpdateCab(self):
-        with patch('Admin.sqlite3') as test:
+        with patch('Admin') as test:
             test.connect().cursor().fetchall().return_value = ['cab.db']
             X = bool(Admin.update_cab)
             assert(X, "Success")
@@ -36,25 +36,25 @@ class Test_admin(unittest.TestCase):
             assert(X, "Success")
 
     def testUpdateEmployee(self):
-        with patch('Admin.sqlite3') as test:
+        with patch('Admin') as test:
             test.connect().cursor().fetchall().return_value = ['cab.db']
             X = bool(Admin.update_employee)
             assert(X, "Success")
 
     def testDeleteEmployee(self):
-        with patch('Admin.sqlite3') as test:
+        with patch('Admin') as test:
             test.connect().cursor().fetchall().return_value = ['cab.db']
             X = bool(Admin.delete_employee)
             assert(X, "Success")
 
     def testShowEmployess(self):
-        with patch('Admin.sqlite3') as test:
+        with patch('Admin') as test:
             test.connect().cursor().fetchall().return_value = ['cab.db']
             X = bool(Admin.show_employees)
             assert(X, "Success")
 
     def testVisualize(self):
-        with patch('Admin.sqlite3') as test:
+        with patch('Admin') as test:
             test.connect().cursor().fetchall().return_value = ['cab.db']
             X = bool(Admin.visualize)
             assert(X, "Success")
